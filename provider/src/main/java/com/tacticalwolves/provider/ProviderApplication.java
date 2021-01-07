a3e898c435cbdc773cad0abf695ccd9f22713e1c
MarcoWeel <deathstarmarco@gmail.com>
2021-01-07 14:47:41 +0100

    changed dependency

--- provider/src/main/java/com/tacticalwolves/provider/ProviderApplication.java
+++ provider/src/main/java/com/tacticalwolves/provider/ProviderApplication.java
@@ -1,10 +1,11 @@
 package com.tacticalwolves.provider;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProviderApplication {
 
     public static void main(String[] args) {
